688f7f6a21c2234bd3927f26d712d54881b2719e
xinz-ctr <i.kamuzinzi@alustudent.com>
2026-02-26 02:48:34 -0800

    .

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+
+if __name__ == "__main__":
+    count = len(sys.argv) - 1
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+    for i in range(1, count + 1):
+        print("{}: {}".format(i, sys.argv[i]))
