3103fcd40cbb3359610f20ef264110ca3608b106
xinz-ctr <i.kamuzinzi@alustudent.com>
2026-02-26 02:18:47 -0800

    .

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print("{}".format(result))
